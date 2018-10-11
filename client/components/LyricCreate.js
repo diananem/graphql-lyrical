2c2de70732ba2fd168f9c4627253ba438caeacf5
diananem <diana.nemyrovska@gmail.com>
2018-10-11 21:03:56 +0300

    Created Song Detail page

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+
+class LyricCreate extends Component {
+  state = {
+    content: ""
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      content: e.target.value
+    });
+  };
+
+  submitLyric = e => {
+    e.preventDefault();
+
+    this.props.mutate({
+      variables: {
+        content: this.state.content,
+        songId: this.props.songId
+      }
+    });
+    this.setState({ content: "" });
+  };
+  render() {
+    console.log(this.state.content);
+    return (
+      <form onSubmit={this.submitLyric}>
+        <label>Add a Lyric</label>
+        <input onChange={this.handleInputChange} value={this.state.content} />
+      </form>
+    );
+  }
+}
+
+const mutation = gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        id
+        content
+      }
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricCreate);
